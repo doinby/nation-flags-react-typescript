d0acad27f8e1291966b2976850879cb38e353e50
Chinh Do <chinh@doinby.co>
2022-08-18 12:55:49 +1000

    Add FontAwesomeIcon. Setup base theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Nunito Sans', 'sans-serif'],
+    },
+    colors: {
+      'dark-blue': '#2B3945',
+      'darker-blue': '#202C37',
+      'darkest-blue': '#111517',
+      'dark-gray': '#858585',
+      'light-gray': '#FAFAFA',
+      white: '#FFFFF',
+      black: '#00000',
+    },
     extend: {},
   },
   plugins: [],
