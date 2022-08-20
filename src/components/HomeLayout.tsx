e11d850816d6f2f18285ffd003f20e204adbfd57
Chinh Do <chinh@doinby.co>
2022-08-20 15:48:26 +1000

    Add dark mode toggle to button

--- src/components/HomeLayout.tsx
+++ src/components/HomeLayout.tsx
@@ -1,27 +1,26 @@
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faMoon, faSun} from '@fortawesome/free-regular-svg-icons';
 import SearchBar from '../components/SearchBar';
 import RegionDropdown from '../components/RegionDropdown';
 import {Outlet} from 'react-router-dom';
+import Header from './Header';
 
 interface Props {
   selectedRegion: {
     name: string;
   };
   setSelectedRegion: any;
+  isDarkMode: boolean;
+  setDarkMode: any;
 }
 
-const HomeLayout = ({selectedRegion, setSelectedRegion}: Props) => {
+const HomeLayout = ({
+  selectedRegion,
+  setSelectedRegion,
+  isDarkMode,
+  setDarkMode,
+}: Props) => {
   return (
     <>
-      <header className='flex justify-between shadow-md px-4 py-8'>
-        <h1>Where in the world?</h1>
-        <section className='flex'>
-          <FontAwesomeIcon icon={faMoon} />
-          <FontAwesomeIcon icon={faSun} />
-          <p>Dark Mode</p>
-        </section>
-      </header>
+      <Header isDarkMode={isDarkMode} setDarkMode={setDarkMode} />
       <main className='flex flex-col items-center'>
         <section id='nav-section' className='flex'>
           <SearchBar />
