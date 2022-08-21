3cc3eb0db50d602549746c36da7b7a350e46bdba
Chinh Do <chinh@doinby.co>
2022-08-21 21:29:22 +1000

    Add country's details from API

--- src/pages/Country.tsx
+++ src/pages/Country.tsx
@@ -3,25 +3,88 @@ import {useParams} from 'react-router-dom';
 import {baseUrl} from '../data/data';
 import useAxiosFetch from '../hooks/useAxiosFetch';
 
-// interface Props {
-//   data: {
-//     name: {common: string};
-//     capital: string;
-//     population: number;
-//     continents: [string];
-//     flags: {png: string};
-//   };
-// }
-
 const Country = () => {
   const {countryId} = useParams();
-  const countryUrl = `${baseUrl}/name/${countryId}/?fullText=true`;
-  const {data, error, isLoading} = useAxiosFetch(countryUrl);
-  console.log('data:', data[0]);
+  const endpoint = countryId && '/name/' + countryId + '?fullText=true';
+  const url = baseUrl + endpoint;
+  const {data, error, isLoading} = useAxiosFetch(url);
 
   return (
     <>
-      <p>Country {countryId}</p>
+      {data &&
+        data.map((country, idx: number) => {
+          if (idx === 0) {
+            const {
+              name: {
+                common: countryName,
+                nativeName: {
+                  ber: {official: officialName},
+                },
+              },
+              tld: {0: topLevelDomain},
+              currencies,
+              languages,
+              capital,
+              population,
+              region,
+              subregion,
+              flags: {png: flagUrl},
+            } = country;
+
+            const isMultiCurrencies = Object.keys(currencies).length > 1;
+            const getCurrencies = () => {
+              let currencyList = '';
+              Object.entries(currencies).forEach((obj1) =>
+                obj1.map((value: any, idx: number) => {
+                  if (idx > 0) {
+                    currencyList += value.name + ', ';
+                  } else return null;
+                })
+              );
+              return currencyList.slice(0, currencyList.length - 2);
+            };
+
+            const isMultiLanguages = Object.keys(languages).length > 1;
+            const getLanguages = () => Object.values(languages).join(', ');
+
+            return (
+              <article key={countryName}>
+                <img src={flagUrl} alt={`Flag of ${countryName}`} />
+                <h3>{countryName}</h3>
+                <div>
+                  <p className='font-semibold'>
+                    Native Name:{' '}
+                    <span className='font-light'>{officialName}</span>
+                  </p>
+                  <p className='font-semibold'>
+                    Population: <span className='font-light'>{population}</span>
+                  </p>
+                  <p className='font-semibold'>
+                    Region: <span className='font-light'>{region}</span>
+                  </p>
+                  <p className='font-semibold'>
+                    Sub Region: <span className='font-light'>{subregion}</span>
+                  </p>
+                  <p className='font-semibold'>
+                    Capital: <span className='font-light'>{capital}</span>
+                  </p>
+                  <p className='font-semibold'>
+                    Top Level Domain:{' '}
+                    <span className='font-light'>{topLevelDomain}</span>
+                  </p>
+                  <p className='font-semibold'>
+                    {isMultiCurrencies ? 'Currencies' : 'Currency'}:{' '}
+                    <span className='font-light'>{getCurrencies()}</span>
+                  </p>
+                  <p className='font-semibold'>
+                    {isMultiLanguages ? 'Languages' : 'Language'}:{' '}
+                    <span className='font-light'>{getLanguages()}</span>
+                  </p>
+                </div>
+              </article>
+            );
+          } else return null;
+        })}
     </>
   );
 };
