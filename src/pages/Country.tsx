1efd747c13e6d6e481a410093dde7322c75d2ea1
Chinh Do <chinh@doinby.co>
2022-08-21 11:58:31 +1000

    Fix eslint/no-unused-vars

--- src/pages/Country.tsx
+++ src/pages/Country.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import {useParams} from 'react-router-dom';
 import {baseUrl} from '../data/data';
 import useAxiosFetch from '../hooks/useAxiosFetch';
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import useAxiosFetch from '../hooks/useAxiosFetch';
-import {baseUrl} from '../data/data';
 import CountryCard from '../components/CountryCard';
 import CountryList from '../components/CountryList';
 
