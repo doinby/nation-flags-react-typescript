a72603adcfd9ea2e733407e058c1f5c2bd05c0b0
Chinh Do <chinh@doinby.co>
2022-09-08 13:26:48 +1000

    Fix footer layout

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 const Footer = ({theme}: Props) => {
   return (
-    <footer className='bg-white dark:bg-dark-blue dark:text-white justify-center gap-x-8 gap-y-2 flex-col md:flex-row'>
+    <footer className='bg-white dark:bg-dark-blue dark:text-white flex-col flex-wrap md:flex-row justify-center gap-x-8 gap-y-2'>
       <FooterLink
         theme={theme}
         url='https://linkedin.com/in/doinby/'
